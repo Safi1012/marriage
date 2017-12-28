954f1f2c49e4fdb24b6030ed0e28a250035b863f
Arne Maier <Arne_Maier@gmx.de>
2017-12-28 17:27:27 +0100

    First sync to firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,47 @@
 import * as React from 'react';
 import './App.css';
+import * as firebase from 'firebase';
 
 const logo = require('./logo.svg');
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+interface State {
+	date: string;
+}
+interface Props {}
+
+class App extends React.Component<Props, State> {
+
+	constructor(props: Props) {
+		super(props);
+		this.state = { date: '' };
+	}
+
+	componentDidMount() {
+		const database = firebase.database ? firebase.database() : undefined;
+		if (database) {
+			const rootRef = database.ref().child('date');
+			rootRef.on('value', (snap) => {
+				this.setState({
+					date: snap ? snap.val() : '',
+				});
+			});
+		}
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<div className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h2>Welcome to React</h2>
+				</div>
+				<p className="App-intro">
+					To get started, edit <code>src/App.tsx</code> and save to reload.
+					{this.state.date}
+				</p>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,20 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import * as firebase from 'firebase';
+
+const config = {
+	apiKey: 'AIzaSyD1h-noaQWGGGgt5U9dblVihVVFvtwOqTE',
+	authDomain: 'marriage-test.firebaseapp.com',
+	databaseURL: 'https://marriage-test.firebaseio.com',
+	projectId: 'marriage-test',
+	storageBucket: 'marriage-test.appspot.com',
+	messagingSenderId: '757149552794',
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(
-  <App />,
+	<App />,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
