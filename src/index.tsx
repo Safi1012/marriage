5e7e3ed94980ba7622ab2db8cbee47979ff485fb
Arne Maier <Arne_Maier@gmx.de>
2017-12-30 18:32:31 +0100

    Add normalize css

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,10 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './features/App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
 import { initializeApp } from 'firebase';
 import { Provider } from 'react-firebase';
+
+import 'normalize.css';
 
 const config = {
 	apiKey: 'AIzaSyD1h-noaQWGGGgt5U9dblVihVVFvtwOqTE',
