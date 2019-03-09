33cbccaf4f7b6cb6418cf8b313850e8143e8fa75
Filipe Santos Correa <filipe.santoscorrea@gmail.com>
2019-03-09 19:15:25 +0100

    Add colors to theme

--- src/common/theme.tsx
+++ src/common/theme.tsx
@@ -4,6 +4,8 @@ export default {
 		primaryColorDark: '#7A0000',
 		secondaryColor: '#5550bd',
 		textColor: '#212529',
+		textColorLight: '#dee2e6',
+		successColor: '#2f9e44',
 		otherColor: '#316F74',
 	},
 };
